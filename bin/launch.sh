56d30e081500003a585c1902346530c96b8c9f29
James Moey <james.moey@news.com.au>
2015-05-01 11:49:16 +1000

    Accept HTTP_PROXY and HTTPS_PROXY on launch

--- bin/launch.sh
+++ bin/launch.sh
@@ -10,6 +10,14 @@ source $BIN_DIRECTORY/install/toran.sh
 source $BIN_DIRECTORY/install/nginx.sh
 source $BIN_DIRECTORY/install/ssh.sh
 
+if [ $HTTP_PROXY ]; then
+	echo "env[HTTP_PROXY] = $HTTP_PROXY" > /etc/php5/fpm/pool.d/www.conf
+fi
+
+if [ $HTTPS_PROXY ]; then
+	echo "env[HTTPS_PROXY] = $HTTPS_PROXY" > /etc/php5/fpm/pool.d/www.conf
+fi
+
 # Start PHP-FPM
 echo "Starting PHP-FPM..."
 php5-fpm -R
