c2e2de0baaa5ff4222aeb9101c1a20e04b4dbc68
James Moey <james.moey@news.com.au>
2015-05-01 15:28:16 +1000

    Append instead of overwrite proxy setting

--- bin/launch.sh
+++ bin/launch.sh
@@ -11,11 +11,11 @@ source $BIN_DIRECTORY/install/nginx.sh
 source $BIN_DIRECTORY/install/ssh.sh
 
 if [ $HTTP_PROXY ]; then
-	echo "env[HTTP_PROXY] = $HTTP_PROXY" > /etc/php5/fpm/pool.d/www.conf
+	echo "env[HTTP_PROXY] = $HTTP_PROXY" >> /etc/php5/fpm/pool.d/www.conf
 fi
 
 if [ $HTTPS_PROXY ]; then
-	echo "env[HTTPS_PROXY] = $HTTPS_PROXY" > /etc/php5/fpm/pool.d/www.conf
+	echo "env[HTTPS_PROXY] = $HTTPS_PROXY" >> /etc/php5/fpm/pool.d/www.conf
 fi
 
 # Start PHP-FPM
