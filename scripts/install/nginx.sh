ac66b29677ebc15249f53c57e3f75181e21bd2b7
Jeremy Zahner <zahner@joshmartin.ch>
2016-04-26 17:01:46 +0200

    Integration of reverse proxy functionality.

--- scripts/install/nginx.sh
+++ scripts/install/nginx.sh
@@ -5,27 +5,40 @@ echo "Loading Nginx vhosts..."
 rm -f /etc/nginx/sites-enabled/*
 if [ "${TORAN_HTTPS}" == "true" ]; then
 
-    echo "Loading HTTPS Certificates..."
+    if [ "${TORAN_REVERSE}" == "false" ]; then
 
-    if [ ! -e "$DATA_DIRECTORY/certs/toran-proxy.key" ]; then
-        echo "ERROR: "
-        echo "  Please add toran-proxy.key in folder certs/"
-        exit 1
+        echo "Loading HTTPS Certificates..."
+
+        if [ ! -e "$DATA_DIRECTORY/certs/toran-proxy.key" ]; then
+            echo "ERROR: "
+            echo "  Please add toran-proxy.key in folder certs/"
+            exit 1
+        fi
+
+        if [ ! -e "$DATA_DIRECTORY/certs/toran-proxy.crt" ]; then
+            echo "ERROR: "
+            echo "  Please add toran-proxy.crt in folder certs/"
+            exit 1
+        fi
+
+        # Add certificates trusted
+        ln -s $DATA_DIRECTORY/certs /usr/local/share/ca-certificates/toran-proxy
+        update-ca-certificates
+
     fi
 
-    if [ ! -e "$DATA_DIRECTORY/certs/toran-proxy.crt" ]; then
-        echo "ERROR: "
-        echo "  Please add toran-proxy.crt in folder certs/"
-        exit 1
+    if [ "${TORAN_REVERSE}" == "true" ]; then
+        ln -s /etc/nginx/sites-available/toran-proxy-https.conf /etc/nginx/sites-enabled/toran-proxy-https-reverse.conf
+    else
+        ln -s /etc/nginx/sites-available/toran-proxy-https.conf /etc/nginx/sites-enabled/toran-proxy-https.conf
     fi
-
-    # Add certificates trusted
-    ln -s $DATA_DIRECTORY/certs /usr/local/share/ca-certificates/toran-proxy
-    update-ca-certificates
-
-    ln -s /etc/nginx/sites-available/toran-proxy-https.conf /etc/nginx/sites-enabled/toran-proxy-https.conf
 else
-    ln -s /etc/nginx/sites-available/toran-proxy-http.conf /etc/nginx/sites-enabled/toran-proxy-http.conf
+
+    if [ "${TORAN_REVERSE}" == "true" ]; then
+        ln -s /etc/nginx/sites-available/toran-proxy-https.conf /etc/nginx/sites-enabled/toran-proxy-http-reverse.conf
+    else
+        ln -s /etc/nginx/sites-available/toran-proxy-http.conf /etc/nginx/sites-enabled/toran-proxy-http.conf
+    fi
 fi
 
 # Logs
